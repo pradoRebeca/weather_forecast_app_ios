168490516a2103843586becc3a911282ebb0445d
Rebeca Nascimento <rebeca.prado@elo7.com>
2025-10-14 19:03:29 -0300

    feat: project init

---
+++ weather app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weather app
+//
+//  Created by Rebeca Nascimento on 14/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = UIColor.red
+    }
+
+
+}
+
